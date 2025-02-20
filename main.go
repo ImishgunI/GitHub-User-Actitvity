e77a2125ce9ef7e2b63ceea6bd74a9ed4e766036
Michael <mvasilyev04@gmail.com>
2025-02-20 21:50:41 +0300

    implement get request

--- main.go
+++ main.go
@@ -1,24 +1,23 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
 	"log"
+	"net/http"
 	"os"
 )
 
-type Event struct {
-	Id    int    `json:"id"`
-	Type  string `json:"type"`
-	Actor struct {
-		Id            int    `json:"id"`
-		Login         string `json:"login"`
-		Display_login string `json:"display_login"`
-	}
-}
+var mp []map[string]any
 
 func main() {
 	username := getUsername()
-	fmt.Print(username)
+	data := jsonHandle(username)
+	parseToMap(data)
+	if payload, ok := mp[0]["payload"].(map[string]any); ok {
+		fmt.Print(payload["ref_type"])
+	}
 }
 
 func getUsername() string {
@@ -26,4 +25,33 @@ func getUsername() string {
 		log.Fatal("You must write a username")
 	}
 	return os.Args[1]
+}
+
+func jsonHandle(username string) []byte {
+	client := &http.Client{}
+	url := fmt.Sprintf("https://api.github.com/users/%s/events", username)
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	req.Header.Add("Accept", "application/vnd.github+json")
+
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	data, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return data
+}
+
+func parseToMap(data []byte) {
+	err := json.Unmarshal(data, &mp)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
